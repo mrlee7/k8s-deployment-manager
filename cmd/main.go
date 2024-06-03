ae155cddeb327f654e1615b32e0655f69c64bc03
jplee227 <jplee227@gmail.com>
2024-06-03 22:43:17 +0900

    config, routing 을 수행하는 main 패키지 작성 - 서버 구동을 위해 handlers - k8s 구동을 하는 코드를 작성합니다. - 예시이기에, well-known port 8080으로 실행합니다.

---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"k8s-deployment-manager/handlers"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+var clientset *kubernetes.Clientset
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	handlers.Initialize(clientset)
+
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/create", handlers.CreateHandler)
+	http.HandleFunc("/update", handlers.UpdateHandler)
+	http.HandleFunc("/list", handlers.ListHandler)
+	http.HandleFunc("/delete", handlers.DeleteHandler)
+
+	log.Println("Server running at http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
+}
